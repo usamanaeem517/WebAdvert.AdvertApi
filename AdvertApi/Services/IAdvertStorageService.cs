db20ce11d90cce1cc8a2980443985dc3f11d5a4a
usamanaeem517 <usamanaeem517@gmail.com>
2022-07-25 14:47:16 +0500

    Added Advert Api and Services and Models

---
+++ AdvertApi/Services/IAdvertStorageService.cs
@@ -0,0 +1,11 @@
+﻿using AdvertApi.Models;
+
+namespace AdvertApi.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+
+    }
+}
